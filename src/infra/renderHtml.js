e89436787c15faf2bcde8584b44e6c7506f88a35
Nicolai Parlog <nipa@codefx.org>
2019-12-18 16:54:53 +0100

    [Site] Use Gatsby's <Link> component for internal links in blog posts

--- src/infra/renderHtml.js
+++ src/infra/renderHtml.js
@@ -1,6 +1,8 @@
 import React from "react"
 import RehypeReact from "rehype-react"
 
+import Link from "../components/link"
+
 const RenderHtml = ({ withAst, htmlAst }) => {
 	const renderAst = new RehypeReact({
 		// this is needed to prevent Rehype from creating a <div> around
@@ -8,9 +10,16 @@ const RenderHtml = ({ withAst, htmlAst }) => {
 		// more: https://github.com/rehypejs/rehype-react/issues/9
 		Fragment: React.Fragment,
 		createElement: React.createElement,
+		components: {
+			a: renderLink,
+		},
 	}).Compiler
 
 	return renderAst(withAst ? withAst.htmlAst : htmlAst)
+}
+
+const renderLink = props => {
+	return <Link to={props.href} children={props.children} />
 }
 
 export default RenderHtml
