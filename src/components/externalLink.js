b095c68ca29454c2578d2565254c2dbeb26ae6e5
Nicolai Parlog <nipa@codefx.org>
2020-01-13 18:35:32 +0100

    [Site] Improve `className` handling

--- src/components/externalLink.js
+++ src/components/externalLink.js
@@ -2,7 +2,7 @@ import React from "react"
 
 // maybe prefix with https://commons.wikimedia.org/wiki/File:External.svg ?
 const ExternalLink = ({ to, className, children }) => {
-	className = className ? className : ""
+	className = className || ""
 	return (
 		<a className={className} rel="noopener noreferrer" href={to}>
 			{children}
--- src/components/link.js
+++ src/components/link.js
@@ -3,18 +3,20 @@ import { Link as Internal } from "gatsby"
 
 import ExternalLink from "./externalLink"
 
-const Link = ({ to, children }) => {
+const Link = ({ to, children, className }) => {
+	className = className || ""
+
 	const external =
 		to.includes("://") || to.startsWith("//") || to.startsWith("mailto")
-	if (external) return <ExternalLink to={to}>{children}</ExternalLink>
+	if (external) return <ExternalLink to={to} className={className}>{children}</ExternalLink>
 
 	const id = to.startsWith("#")
-	if (id) return <a href={to}>{children}</a>
+	if (id) return <a href={to} className={className}>{children}</a>
 
 	// if internal links don't start with "/", Gatsby emits a warning;
 	// prevent that by prefixing internal links with a "/" if they lack one
 	to = to.startsWith("/") ? to : `/${to}`
-	return <Internal to={to}>{children}</Internal>
+	return <Internal to={to} className={className}>{children}</Internal>
 }
 
 export default Link
