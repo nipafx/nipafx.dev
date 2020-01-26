2ede4506eb6acac19b49e04146309aea85189308
Nicolai Parlog <nipa@codefx.org>
2020-01-26 13:18:34 +0100

    [Site] Improve wording in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -28,14 +28,17 @@ const FourOhFourPage = () => (
 					I'm terribly sorry this happened and I really hope you can find what you're
 					looking for - on this blog, but also in life. It's not easy to achieve your
 					goals and broken sites throwing a wrench into your daily business aren't exactly
-					helping. But look at the bright side: You're out and about learning about
-					Java and that's pretty cool!
+					helping.
 				</p>
 				<p>
-					I'd love to help you as much as I can. If you reach out to me{" "}
-					<Link to={"https://twitter.com/nipafx"}>on Twitter</Link> or{" "}
-					<Link to={"mailto:foo@bar.de?subject=404 on nipafx.dev&"}>via mail</Link> and
-					let me know what you were looking for, I'll find it and get back to you ASAP.
+					But look at the bright side: You're out and about learning about Java and that's
+					pretty cool! I'd love to help you with that as much as I can: If you reach out
+					to me <Link to={"https://twitter.com/nipafx"}>on Twitter</Link> or{" "}
+					<Link to={"mailto:nicolai@nipafx.dev?subject=404 on nipafx.dev&"}>
+						via mail
+					</Link>{" "}
+					and let me know what you were looking for, I'll find it and get back to you
+					ASAP.
 				</p>
 			</section>
 		</main>
