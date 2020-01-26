f040976a92d6aed919c92614c9335d40661d3a08
Nicolai Parlog <nipa@codefx.org>
2020-01-26 13:07:54 +0100

    [Site] Don't try to parse React components as Markdown
    Before this change, React components that were passed as post header titles (e.g. fot tag pages) were not displayed.

--- src/infra/mdAsHtml.js
+++ src/infra/mdAsHtml.js
@@ -4,6 +4,10 @@ import remark from "remark"
 import remarkHTML from "remark-html"
 
 const MdAsHtml = ({ children }) => {
+	// check whether `children` are React elements
+	if (children.$$typeof)
+		return children
+
 	const __html =
 		children instanceof Array ? children.map(md => toHtml(md)).join("") : toHtml(children)
 	return <span dangerouslySetInnerHTML={{ __html }} />
