fdd146cbf0da820a8bce6b66621c408f7ad6e8bc
Nicolai Parlog <nipa@codefx.org>
2020-02-20 02:53:19 +0100

    [Site] Let uplink'ed taglets update hash instead of linking to the taglet site

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,44 +1,75 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
-import { classNames } from "../infra/functions"
+import { classNames, tagletsFromHash } from "../infra/functions"
 
 import Link from "./link"
 
 import style from "./tag.module.css"
 
-export const Tag = ({ tag, link, className }) => {
-	className = className || ""
+export const Tag = ({ tag, link, uplink, className }) => {
 	// replace hyphen with non-breaking hyphen
-	const tagText = tag.replace("-", "‑")
+	const tagText = tag === "all" ? "ALL-TAGS" : tag.replace("-", "‑")
 	return (
-		<span {...classNames(className, style.tag)}>
-			{link ? <Link to={tag}>#{tagText}</Link> : `#${tagText}`}
+		<span data-tag={tag} {...classNames(className, style.tag)}>
+			{uplink ? (
+				<Link to={tag} onClick={event => handleClick("tag", tag, event)}>
+					#{tagText}
+				</Link>
+			) : link ? (
+				<Link to={tag}>#{tagText}</Link>
+			) : (
+				`#${tagText}`
+			)}
 		</span>
 	)
 }
 
-export const Channel = ({ channel, plural, link, className }) => {
-	className = className || ""
+export const Channel = ({ channel, plural, link, uplink, colorize, className }) => {
+	const classes = [style.channel]
+	if (colorize) classes.push(style.colorize)
+	if (className) classes.push(className)
+
 	const { singularName, pluralName, slug } = channelInfo(channel)
 	const name = plural ? pluralName : singularName
 	return (
-		<span {...classNames(className, style.channel)}>
-			{link ? <Link to={slug}>#{name}</Link> : `#${name}`}
+		<span data-channel={channel} {...classNames(...classes)}>
+			{uplink ? (
+				<Link to={slug} onClick={event => handleClick("channel", channel, event)}>
+					#{name}
+				</Link>
+			) : link ? (
+				<Link to={slug}>#{name}</Link>
+			) : (
+				`#${name}`
+			)}
 		</span>
 	)
 }
 
+const handleClick = (kind, taglet, event) => {
+	event.preventDefault()
+	tagletsFromHash()
+		.toggleSelection(kind, taglet)
+		.setToHash()
+}
+
 const channelInfo = channel =>
-	useStaticQuery(graphql`
-		query {
-			channels: allChannel {
-				nodes {
-					internalName
-					singularName
-					pluralName
-					slug
+	channel === "all"
+		? {
+				singularName: "ALL",
+				pluralName: "ALL-CHANNELS",
+				slug: "/",
+		  }
+		: useStaticQuery(graphql`
+				query {
+					channels: allChannel {
+						nodes {
+							internalName
+							singularName
+							pluralName
+							slug
+						}
+					}
 				}
-			}
-		}
-	`).channels.nodes.find(node => node.internalName === channel)
+		  `).channels.nodes.find(node => node.internalName === channel)
--- src/infra/functions.js
+++ src/infra/functions.js
@@ -15,6 +15,76 @@ export function classNames() {
 		className: Array.prototype.slice
 			.call(arguments)
 			.filter(cls => cls !== undefined && cls !== null && cls !== "")
-			.join(" ")
+			.join(" "),
+	}
+}
+
+export function tagletsFromHash() {
+	const hash = (window.location.hash || "").replace("#", "")
+
+	const allChannels = !hash.includes("channels__")
+	const allTags = !hash.includes("tags__")
+	const channels = allChannels
+		? []
+		: hash
+				.replace("channels__", "")
+				.replace(/___tags__.*/, "")
+				.split("_")
+	const tags = allTags
+		? []
+		: hash
+				.replace(/channels__.*___/, "")
+				.replace("tags__", "")
+				.split("_")
+	return {
+		_channels: {
+			all: allChannels,
+			entries: channels,
+		},
+
+		_tags: {
+			all: allTags,
+			entries: tags,
+		},
+
+		toggleSelection: function(kind, taglet) {
+			const taglets = this[`_${kind}s`]
+			if (taglet === "all") {
+				taglets.all = true
+				taglets.entries = []
+			} else {
+				const index = taglets.entries.indexOf(taglet)
+				if (index >= 0) taglets.entries.splice(index, 1)
+				else {
+					taglets.entries.push(taglet)
+					taglets.entries.sort()
+				}
+				taglets.all = taglets.entries.length === 0
+			}
+			return this
+		},
+
+		setToHash: function() {
+			const channelHash = this._channels.all
+				? null
+				: "channels__" + this._channels.entries.join("_")
+			const tagHash = this._tags.all ? null : "tags__" + this._tags.entries.join("_")
+
+			let hash = channelHash
+			if (tagHash)
+				if (hash) hash += "___" + tagHash
+				else hash = tagHash
+
+			if (hash) window.location.hash = hash
+			else {
+				window.location.hash = ""
+				// remove the hash from the URL that the previous line left behind
+				window.history.pushState(
+					"",
+					document.title,
+					window.location.pathname + window.location.search
+				)
+			}
+		},
 	}
 }
