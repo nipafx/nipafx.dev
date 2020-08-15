fbeafad71d5368d0e6bfd20cb2ad007111187f3a
Nicolai Parlog <nipa@codefx.org>
2020-08-15 14:30:26 +0200

    [Site] Enable link list to display icons

--- src/components/linkList.js
+++ src/components/linkList.js
@@ -1,19 +1,36 @@
 import React from "react"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 import { classNames } from "../infra/functions"
 
 import Link from "./link"
 
 import style from "./linkList.module.css"
 
-const LinkList = ({ className, links }) => (
-	<div {...classNames(className, style.container)}>{links.map(link)}</div>
-)
+const LinkList = ({ showIcons, links, className }) => {
+	const styles = [className]
+	if (showIcons) styles.push(style.icons)
+	else styles.push(style.text)
+	return (
+		<span {...classNames(...styles)}>
+			{links.map(link => selectDisplay(showIcons, link)).map(showLink)}
+		</span>
+	)
+}
 
-const link = ({ title, url, className }) => (
+const selectDisplay = (showIcons, { title, fontAwesome, url, className }) => {
+	return {
+		element: showIcons && fontAwesome ? <FontAwesomeIcon icon={fontAwesome} /> : title,
+		url,
+		className,
+	}
+}
+
+const showLink = ({ element, url, className }) => (
 	<span key={url}>
 		<Link to={url} className={className}>
-			{title}
+			{element}
 		</Link>
 	</span>
 )
