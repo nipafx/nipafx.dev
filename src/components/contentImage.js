e66824c3f15d11946c7938ec1f72ec89bbebeeb1
Nicolai Parlog <nipa@codefx.org>
2020-09-21 22:38:20 +0200

    [Site] Adjust wideness of content images/videos

--- src/components/contentImage.js
+++ src/components/contentImage.js
@@ -11,12 +11,12 @@ const ContentImage = ({ slug, options, className }) => {
 	options = options || ""
 	let type = "content"
 	const divClasses = [className]
-	const imageClasses = []
+	const imageClasses = [style.image]
 
-	if (options.includes("wide")) {
-		divClasses.push(layout.wide)
-		divClasses.push(style.margins)
-	}
+	// would be nice to add the two classes above
+	// and remove them here if `narrow` is in the options,
+	// but apparently JS is too cool for Array::remove 🙄
+	if (!options.includes("narrow")) divClasses.push(layout.wide)
 	if (options.includes("sidebar")) {
 		divClasses.push(layout.sidebar)
 		type = "sidebar"
--- src/components/contentVideo.js
+++ src/components/contentVideo.js
@@ -9,15 +9,12 @@ import style from "./contentVideo.module.css"
 
 const ContentVideo = ({ slug, options, className }) => {
 	options = options || ""
-	const classes = [className]
+	const classes = [className, style.video]
 
 	// would be nice to add the two classes above
 	// and remove them here if `narrow` is in the options,
 	// but apparently JS is too cool for Array::remove 🙄
-	if (!options.includes("narrow")) {
-		classes.push(layout.wide)
-		classes.push(style.margins)
-	}
+	if (!options.includes("narrow")) classes.push(layout.wide)
 
 	return <Video slug={slug} {...classNames(...classes)} />
 }
