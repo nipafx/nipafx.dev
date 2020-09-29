92bf480a85360c0f113c3001b008bfa1220951c7
Nicolai Parlog <nipa@codefx.org>
2020-09-29 02:12:22 +0200

    [Site] Improve Browser compatibility
    The browserslist configuration is the default. It is repeated here
    to remind me that it plays a role.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,12 @@ import "./src/layout/global.css"
 import "./src/layout/colors.css"
 import "./src/layout/fonts.css"
 import "./src/layout/code.css"
+
+// https://www.gatsbyjs.com/plugins/gatsby-background-image/#important
+// IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+export const onClientEntry = () => {
+	if (!(`IntersectionObserver` in window)) {
+		import(`intersection-observer`)
+		console.log(`# IntersectionObserver is polyfilled!`)
+	}
+}
