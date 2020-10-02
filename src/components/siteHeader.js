d3bae43016841d2fa3040fdb7d420a921e7b9ba9
Nicolai Parlog <nipa@codefx.org>
2020-10-02 15:36:15 +0200

    [Site] Scroll header out of view on small screens

--- src/components/siteHeader.js
+++ src/components/siteHeader.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
 
 import { classNames } from "../infra/functions"
 
@@ -12,15 +12,39 @@ import channels from "../../content/meta/channel-links.json"
 import layout from "../layout/container.module.css"
 import style from "./siteHeader.module.css"
 
-const SiteHeader = ({ onIndexPage }) => (
-	<header id={PROGRESS_BAR_OFFSET} {...classNames(layout.container, style.header)}>
-		<div {...classNames(layout.siteHeader, style.container)}>
-			<SiteLogo className={style.logo} onIndexPage={onIndexPage} />
-			<LinkList showIconsUntil={600} className={style.channels} links={channels.links} />
-			<SiteMenu className={style.menu} onIndexPage={onIndexPage} />
-		</div>
-		<ProgressBar className={layout.fullWidth} />
-	</header>
-)
+const SiteHeader = ({ onIndexPage }) => {
+	const scrollPosition = useRef(0)
+	const headerElement = useRef(null)
+	useEffect(() => {
+		initializeHeaderVisibility(scrollPosition, headerElement)
+		const scrollListener = () => updateHeaderVisibility(scrollPosition, headerElement)
+		window.addEventListener("scroll", scrollListener)
+		return () => {
+			window.removeEventListener("scroll", scrollListener)
+		}
+	})
+	return (
+		<header id={PROGRESS_BAR_OFFSET} {...classNames(layout.container, style.header)}>
+			<div {...classNames(layout.siteHeader, style.container)}>
+				<SiteLogo className={style.logo} onIndexPage={onIndexPage} />
+				<LinkList showIconsUntil={600} className={style.channels} links={channels.links} />
+				<SiteMenu className={style.menu} onIndexPage={onIndexPage} />
+			</div>
+			<ProgressBar className={layout.fullWidth} />
+		</header>
+	)
+}
+
+const initializeHeaderVisibility = (scrollPosition, headerElement) => {
+	scrollPosition.current = window.pageYOffset
+	headerElement.current = document.querySelector("#" + PROGRESS_BAR_OFFSET)
+}
+
+const updateHeaderVisibility = (scrollPosition, headerElement) => {
+	const position = window.pageYOffset
+	if (position < scrollPosition.current) headerElement.current.classList.add(style.show)
+	else headerElement.current.classList.remove(style.show)
+	scrollPosition.current = position
+}
 
 export default SiteHeader
