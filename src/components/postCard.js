74d3b8175f0bc622e5a795629d444cf227722d50
Nicolai Parlog <nipa@codefx.org>
2020-12-01 01:15:55 +0100

    [Site] Fix broken filter when clicking a tag #18

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -12,28 +12,34 @@ import tagletStyle from "./taglet.module.css"
 
 const PostCard = ({ title, slug, date, channel, tags, description, featuredImage, className }) => {
 	return (
-		<BackgroundImage
+		// without this outer div, there's a problem with filtering:
+		// when on a post page and you click a tag, the landing page will show
+		// and URL and filter highlighting are correct, but all posts are shown
+		// (I assume it has something to do with how <BackgroundImage> handles
+		// CSS classes, but I don't know for sure)
+		<div
 			data-channel={channel}
 			data-tags={tags}
 			{...classNames(style.card, channel, className)}
-			fluid={featuredImage.fluid}
 		>
-			<Link to={slug} className={style.content}>
-				<div className={style.top}>
-					<span className={style.title} dangerouslySetInnerHTML={{ __html: title }} />
-					<Channel channel={channel} colorize className={style.channel} />
-					{/* it would be clearer to use <Tag>s instead of concatenating strings and
+			<BackgroundImage fluid={featuredImage.fluid} className={style.image}>
+				<Link to={slug} className={style.content}>
+					<div className={style.top}>
+						<span className={style.title} dangerouslySetInnerHTML={{ __html: title }} />
+						<Channel channel={channel} colorize className={style.channel} />
+						{/* it would be clearer to use <Tag>s instead of concatenating strings and
 						applying the correct style, but that requires more DOM nodes */}
-					<span {...classNames(tagletStyle.taglet, style.tags)}>
-						{tags.map(tagletText).join(" ")}
-					</span>
-				</div>
-				<p className={style.description}>
-					<span dangerouslySetInnerHTML={{ __html: description }} />
-					<FormattedDate date={date} className={style.date} />
-				</p>
-			</Link>
-		</BackgroundImage>
+						<span {...classNames(tagletStyle.taglet, style.tags)}>
+							{tags.map(tagletText).join(" ")}
+						</span>
+					</div>
+					<p className={style.description}>
+						<span dangerouslySetInnerHTML={{ __html: description }} />
+						<FormattedDate date={date} className={style.date} />
+					</p>
+				</Link>
+			</BackgroundImage>
+		</div>
 	)
 }
 
