a6deea33c663ff648754f701abb41b6a186a39e1
Nicolai Parlog <nipa@codefx.org>
2021-05-18 14:51:39 +0200

    [Event] Announce 26hJava

---
+++ content/stubs/26-hours-java.md
@@ -0,0 +1,43 @@
+---
+title: "26 Hours of Java"
+isPost: true
+channel: "events"
+structuredDataType: "event"
+tags: [conversation, community]
+date: 2021-05-18
+slug: 26h-java
+description: "On May 29th we'll throw a late birthday party for Java, which turned 26 a few days before. With a 26-hour live stream relay race! 🥳"
+featuredImage: 25h-java
+---
+
+Yes, 26 hours of Java!
+There will be technical deep dives, interviews, conversations, and lots of code!
+Most importantly, it will be a lot of fun and I hope you join us on that fine Saturday in May.
+
+
+## Time
+
+Same time like [last year](25h-java) (but an hour longer of course!):
+
+*   where Java was born: **11 PM (28th) to 1 AM (30th) PDT**
+*   where the clock lives: **0600 (29th) to 0800 (30th) UTC**
+*   where I live: **0800 (29th) to 1000 (30th) CEST**
+
+
+## Locations
+
+This year, I won't be doing it alone (we're all getting old, even the Mona Lisa is falling apart).
+Instead, there are a few other cool Java streamers who will take over some of those 26 hours:
+
+* Sebastien Blanc [on YouTube](https://www.youtube.com/user/sebi2706)
+* Billy Korando [on YouTube](https://www.youtube.com/watch?v=4e2wwQgFc2E)
+* Ted M. Young [on Twitch](https://www.twitch.tv/jitterted)
+* yours truly [on Twitch](https://www.twitch.tv/nipafx)
+
+Think of this as a relay race - each of us will stream on their own channel and hand over the baton to the next when the time comes.
+
+
+## Timeline
+
+Here's what we plan to dissect and discuss with you.
+We will have a few slides, guests, repos, and a rough idea where we're going, but the cool thing about a stream is that you're there with us, so you can ask questions, decide what to emphasize, and tell us where to go next.
---
+++ src/pages/26h-java.js
@@ -0,0 +1,205 @@
+import React from "react"
+
+import stub from "../infra/stubs"
+import { classNames } from "../infra/functions"
+
+import PostLayout from "../layout/post"
+import PostContent from "../components/postContent"
+import SiteLayout from "../layout/site"
+import MarkdownAsHtml from "../infra/markdownAsHtml"
+
+import * as style from "./26h-java.module.css"
+import * as layout from "../layout/container.module.css"
+
+const Java26hPage = () => {
+	const { meta, header, content } = stub(`26h-java`)
+
+	return (
+		<SiteLayout className="event" meta={meta}>
+			<PostLayout {...header}>
+				<PostContent {...content} />
+				<Schedule />
+			</PostLayout>
+		</SiteLayout>
+	)
+}
+
+const Schedule = () => (
+	<div className={layout.container}>
+		<div {...classNames(style.schedule, layout.siteHeader)}>
+			{entry(
+				"overflow",
+				new Date(2021, 4, 29, 6),
+				new Date(2021, 4, 29, 7),
+				"nipafx",
+				"Java 11-16 // StackOverflow"
+			)}
+			{entry(
+				"pioneer",
+				new Date(2021, 4, 29, 7),
+				new Date(2021, 4, 29, 13),
+				"nipafx",
+				"JUnit Pioneer // hack.commit.push",
+				"Pioneer participates in [hack.commit.push](https://paris2021.hack-commit-pu.sh/) and Nicolai will stream his end of it."
+			)}
+			{entry(
+				"quarkus",
+				new Date(2021, 4, 29, 13),
+				new Date(2021, 4, 29, 14),
+				"sebi",
+				"Discovering Quarkus"
+			)}
+			{entry(
+				"jbang",
+				new Date(2021, 4, 29, 14),
+				new Date(2021, 4, 29, 15),
+				"sebi",
+				"Exploring JBang"
+			)}
+			{entry(
+				"pacman",
+				new Date(2021, 4, 29, 15),
+				new Date(2021, 4, 29, 16),
+				"sebi",
+				"Developing Pacman",
+				"Sebastian has a Pacman clone with a Java backend and cool Kafka stuff that he'll be working on."
+			)}
+			{entry(
+				"ama",
+				new Date(2021, 4, 29, 16),
+				new Date(2021, 4, 29, 17),
+				"nipafx",
+				"Java AMA"
+			)}
+			{entry(
+				"ron",
+				new Date(2021, 4, 29, 17),
+				new Date(2021, 4, 29, 18),
+				"nipafx",
+				"Ron Pressler // State of Loom",
+				"Ron Pressler, Lead of [Project Loom](https://wiki.openjdk.java.net/display/loom/Main), will be here to talk about the project."
+			)}
+			{entry(
+				"maurizio",
+				new Date(2021, 4, 29, 18),
+				new Date(2021, 4, 29, 19),
+				"nipafx",
+				"Maurizio Cimadamore // State of Panama",
+				"Maurizio Cimadamore, Lead of [Project Panama](https://openjdk.java.net/projects/panama/), will be there to talk about the project."
+			)}
+			{entry(
+				"brian",
+				new Date(2021, 4, 29, 19),
+				new Date(2021, 4, 29, 20),
+				"nipafx",
+				"Brian Goetz // State of Amber & Valhalla",
+				"Brian Goetz, Lead of Projects [Amber](https://openjdk.java.net/projects/amber/) and [Valhalla](https://openjdk.java.net/projects/valhalla/), will be there to talk about the projects."
+			)}
+			{entry(
+				"late",
+				new Date(2021, 4, 29, 20),
+				new Date(2021, 4, 29, 21),
+				"nipafx",
+				"T.B.D.",
+				"Not sure yet, but something about Java. Have an idea? [Get in touch!](contact)"
+			)}
+			{entry(
+				"billy",
+				new Date(2021, 4, 29, 21),
+				new Date(2021, 4, 30, 0),
+				"billy",
+				"Spring, Community, Coding",
+				"Billy will have Josh Long and Pratik Patel over to chat and will then do some live-coding."
+			)}
+			{entry(
+				"ted",
+				new Date(2021, 4, 30, 0),
+				new Date(2021, 4, 30, 5),
+				"ted",
+				"Remote Mob Registration",
+				"Ted will work on his _Remote Mob Registration System_, built with Java 16, Spring Boot 2.5, and a Thymeleaf-based front-end. t goes without saying that it was lovingly TDD'd."
+			)}
+			{entry(
+				"closing",
+				new Date(2021, 4, 30, 5),
+				new Date(2021, 4, 30, 8),
+				"nipafx",
+				"T.B.D.",
+				"Not sure yet, but something about Java. Have an idea? [Get in touch!](contact)"
+			)}
+		</div>
+	</div>
+)
+
+const entry = (area, startDate, endDate, streamer, topic, description) => {
+	const { text, url } = streamerLink(streamer)
+	return (
+		<div {...classNames(style.entry, style[streamer])} style={{ gridArea: area }}>
+			<p className={style.date}>
+				<span className={style.startTime}>{time(startDate)}</span>
+				<span className={style.endTime}>{time(endDate, true)}</span>
+			</p>
+			<h3 className={style.topic}>{topic}</h3>
+			<p className={style.description}>
+				{description && <MarkdownAsHtml>{description}</MarkdownAsHtml>}
+			</p>
+			<a className={style.link} href={url}>
+				{text}
+			</a>
+		</div>
+	)
+}
+
+const streamerLink = streamer => {
+	switch (streamer) {
+		case "sebi":
+			return {
+				text: "on Sebastian's YouTube",
+				url: "https://www.youtube.com/user/sebi2706",
+			}
+		case "billy":
+			return {
+				text: "on KCJUG's YouTube",
+				url: "https://www.youtube.com/watch?v=4e2wwQgFc2E",
+			}
+		case "ted":
+			return {
+				text: "on Ted's Twitch",
+				url: "https://www.twitch.tv/jitterted",
+			}
+		default:
+			return {
+				text: "on Nicolai's Twitch",
+				url: "https://twitch.tv/nipafx",
+			}
+	}
+}
+
+const day = date => {
+	switch (date.getDate()) {
+		case 1:
+			return `1st`
+		case 2:
+			return `2nd`
+		case 3:
+			return `3rd`
+		case 21:
+			return `21st`
+		case 22:
+			return `22nd`
+		case 23:
+			return `23rd`
+		case 31:
+			return `31st`
+		default:
+			return date.getDate() + "th"
+	}
+}
+
+const time = (date, showUtc) => {
+	let minutes = date.getMinutes()
+	if (minutes < 10) minutes = "0" + minutes
+	return date.getHours() + ":" + minutes + (showUtc ? " UTC" : "")
+}
+
+export default Java26hPage
